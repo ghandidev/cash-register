906626041aa42c266e9c23e8719a2fd501edcf8b
ghandidev <ghandiaboultaif@gmail.com>
2024-05-10 01:12:37 -0400

    initial commit added html code and styles

---
+++ script.js
@@ -0,0 +1,12 @@
+let price = 1.87;
+let cid = [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100],
+];
